9a81570c1390618d60d44ebc4a03d2ccb1b13cfb
Brexton Pham <brextonpham@stanford.edu>
2015-07-24 11:52:42 -0400

    Converted .xib file to Storyboard

---
+++ IOS7ScrollViews copy/iOS7ScrollViews/SVAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SVAppDelegate.h
+//  iOS7ScrollViews
+//
+//  Created by Pierre Felgines on 20/06/13.
+//  Copyright (c) 2013 Pierre Felgines. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SVAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
